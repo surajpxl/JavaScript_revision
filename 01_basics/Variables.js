43b332ddfe295085e7459832a079b0cb81a56087
surajpxl <guptasuraj4455@gmail.com>
2025-07-07 12:16:28 +0530

    basics variable revision

---
+++ 01_basics/Variables.js
@@ -0,0 +1,13 @@
+const accountId = 334455;
+let accountEmail = "suraj@google.com"
+var accountPassword = "12345";
+accountCity = "Pune"; // This will throw an error because accountCity is not declared with let or var
+
+// accountId = 2 // This will throw an error because accountId is a constant and cannot be reassigned
+
+accountEmail = "sg@gmail.com"; // This is allowed because accountEmail is declared with let
+accountPassword = "67890"; // This is allowed because accountPassword is declared with var
+accountCity = "Mumbai"; // This is allowed because accountCity is not declared with let or var
+
+console.log(accountId, accountEmail, accountPassword, accountCity);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
