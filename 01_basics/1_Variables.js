138be77876a898476946bae708acf97764dd6d6f
surajpxl <guptasuraj4455@gmail.com>
2025-07-07 19:49:57 +0530

    basics variable revision

--- 01_basics/1_Variables.js
+++ 01_basics/1_Variables.js
@@ -16,3 +16,4 @@ because of issue in block scope and functional scope
 
 console.log(accountId, accountEmail, accountPassword, accountCity);
 console.table([accountId, accountEmail, accountPassword, accountCity]);
+
