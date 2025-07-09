5fb33d047fd473488eccdca280642fd0475ec0bd
surajpxl <guptasuraj4455@gmail.com>
2025-07-09 17:49:45 +0530

    Numbers and Math basics in JavaScript

---
+++ 01_basics/7_nums_and_math.js
@@ -0,0 +1,48 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.456789
+// console.log(otherNumber.toPrecision(5)); // 123.46
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
+
+
+
+// ******************* Maths********************
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-5)); // 5
+// console.log(Math.round(5.6)); // 6
+// console.log(Math.ceil(5.1)); // 6
+// console.log(Math.floor(5.9)); // 5
+// console.log(Math.min(2, 3, 1, -5)); // -5
+// console.log(Math.max(2, 3, 1, -5)); // 3
+
+console.log(Math.random()); // Random number between 0 and 1
+console.log((Math.random()*10) + 1); // Random number between 1 and 10
+console.log(Math.floor((Math.random()*10) + 1)); // Random integer between
+
+const min = 10;
+const max = 20;
+
+console.log((Math.random() * (max - min + 1)) + min) // Random number between min and max
+
+
+
+
+
+
+
+
+
+
+
+
