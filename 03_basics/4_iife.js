f6d35cdd9d8d097822eb0c730e8828f986078ff7
surajpxl <guptasuraj4455@gmail.com>
2025-07-17 22:17:26 +0530

    arrow and this

--- 03_basics/4_iife.js
+++ 03_basics/4_iife.js
@@ -1,11 +1,2 @@
-// Immediately Invoked Function Expression (IIFE) Example
 
-(function myFun() {
-    console.log(`db connected`); 
-})();
-
-
-((name) => {
-    console.log(`Hello ${name}`); // IIFE with parameters
-})("Suraj");
 
