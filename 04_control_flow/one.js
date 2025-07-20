19710095455f26bfd1b455e84613e2c98474fe05
surajpxl <guptasuraj4455@gmail.com>
2025-07-20 22:40:44 +0530

    logical condition

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -37,3 +37,18 @@ const temperature = 41
 //     console.log("less than 1200");
 
 // }
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
